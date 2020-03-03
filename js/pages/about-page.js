909079d4cc622e3514bd34a5487dd15356dbd59f
vovaEygel <45798277+vovaEygel@users.noreply.github.com>
2020-03-03 06:32:11 +0200

    Update about-page.js

--- js/pages/about-page.js
+++ js/pages/about-page.js
@@ -2,7 +2,7 @@ export default {
     template: `
     <section class="about">
        <h1>about</h1>
-       <img src="../../img/about.jpg"/>
+       <img src="././img/about.jpg"/>
      </section>
     `
 }
