3f011f5548c8543de0064e1e28d0a707384af7a4
vovaEygel <45798277+vovaEygel@users.noreply.github.com>
2020-03-03 08:35:16 +0200

    Update navbar.cmp.js

--- js/cmps/navbar.cmp.js
+++ js/cmps/navbar.cmp.js
@@ -6,13 +6,13 @@ export default {
       </router-link>
       <div class="nav-links flex align-center" :class="{'menu-open': isMenuOpen}">
         <router-link exact to="/" class="mobile-nav-logo">
-          <img class="logo" src="../../img/appsuslogo.png"/>
+          <img class="logo" src="././img/appsuslogo.png"/>
         </router-link>
         <router-link to="/mr-email" @click.native="closeMobileNav()">Mr Email</router-link>
         <router-link to="/miss-keep" @click.native="closeMobileNav()">Miss Keep</router-link>
         <router-link to="/about" @click.native="closeMobileNav()">About</router-link>
       </div>
-        <img class="hamburger-menu" src="../../img/hamburger.png" @click="toggleMenu">
+        <img class="hamburger-menu" src="././img/hamburger.png" @click="toggleMenu">
     </section>
   `,
 
